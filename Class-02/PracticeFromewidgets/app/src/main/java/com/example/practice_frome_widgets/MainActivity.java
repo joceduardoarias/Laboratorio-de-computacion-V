81ecf0a15ee820cd9501132afa8075efb36e1e81
Jose Arias <josee.ariasmelo@gmail.com>
2023-09-21 11:28:30 -0300

    Add practice proyect class-2

---
+++ Class-02/PracticeFromewidgets/app/src/main/java/com/example/practice_frome_widgets/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.practice_frome_widgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ImageView img= (ImageView) findViewById(R.id.imgFoto);
+        //img.setImageResource(R.drawable.androide);
+    }
+}
